6784f8fc5621c9a21a2909f3d0d0f33be6418419
Cédric Oloa <cedric.oloa@laposte.net>
2019-02-17 23:26:26 +0100

    react setup

---
+++ app/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return <div>This is my App</div>;
+	}
+}
+
+export default App;
