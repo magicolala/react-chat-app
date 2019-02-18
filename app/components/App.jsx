b9c1232ff94eee7d013f236b52b61f0c0d378b18
Cédric Oloa <cedric.oloa@laposte.net>
2019-02-18 01:31:07 +0100

    adding messages

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -8,12 +8,21 @@ import UserForm from './Users/UserForm.jsx';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			status: 'disconnected',
+			messages: [{
+				timeStamp: Date.now,
+				text: "Welcome to SockChat"
+			}],
+			users: [],
+			user: ''
+		};
 	}
 
 	componentWillMount() {
 		this.socket = io('http://localhost:3000');
 		this.socket.on('connect', this.connect.bind(this));
+		this.socket.on('messageAdded', this.onMessageAdded.bind(this));
 	}
 
 	connect() {
@@ -21,15 +30,28 @@ class App extends Component {
 		console.log('Connected: ' + this.socket.id);
 	}
 
+	onMessageAdded(message) {
+		this.setState({messages: this.state.messages.concat(message)});
+	}
+
+	disconnect() {
+		this.setState({status: 'disconnected'});
+	}
+
+	emit(eventName, payload) {
+		this.socket.emit(eventName, payload);
+	}
+
 	render() {
+		console.log(this.state.messages);   
 		return (
 			<div className="row">
 				<div className="col-md-4">
-					<UserList />
+					<UserList {...this.state} />
 				</div>
 				<div className="col-md-8">
-					<MessageList />
-					<MessageForm />
+					<MessageList {...this.state} />
+					<MessageForm {...this.state} emit={this.emit.bind(this)} />
 				</div>
 			</div>
 		);
--- server.js
+++ server.js
@@ -11,6 +11,23 @@ var server = app.listen(3000);
 io = require ('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
+
+    socket.once('disconnect', function() {
+        connections.splice(connections.indexOf(socket), 1);
+        socket.disconnect();
+        console.log('Disconnected: %s sockets connected', connections.length);
+        io.emit('disconnect');
+    });
+
+    socket.on('messageAdded', function(payload) {
+        var newMessage = {
+            timeStamp: payload.timestamp,
+            text: payload.text
+        };
+
+        io.emit('messageAdded', newMessage);
+    });
+
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
 });
